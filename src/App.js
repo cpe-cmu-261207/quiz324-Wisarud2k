55f541c2125eb0ea923a21840081c2575eabeebe
Wisarud2k <59276807+Wisarud2k@users.noreply.github.com>
2021-02-09 14:12:43 +0700

    Sadddddd

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import { useState, useEffect } from "react";
 
 function App() {
   //add useState for all state variables
-
+  const [input, setInput] = useState({ name: "", gender: "", age: "" });
+  const [pet, setPet] = useState([]);
   //load locationStorage
   useEffect(() => {
     const items = localStorage.getItem("items");
     // ...
+    setPet(JSON.parse(items));
   }, []);
 
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(pet));
+  }, [pet]);
+
+  const addform = () => {
+    //e.preventdefault();
+
+    if (input.name !== "" && input.gender !== "" && input.age !== "") {
+      setPet(pet.push(input));
+      setInput({ name: "", gender: "", age: "" });
+      //console.log(pet);
+    }
+  };
   return (
     <div className="card" style={{ width: 400 }}>
       <div className="card-content">
@@ -21,12 +36,22 @@ function App() {
             type="text"
             placeholder="e.q Coco"
             //update related state based on event
+            onChange={(e) =>
+              setInput({ ...input, name: e.currentTarget.value })
+            }
           ></input>
         </div>
 
         <div className="field">
           <label className="label">Gender</label>
-          <select className="input" type="text" placeholder="Please select ..">
+          <select
+            className="input"
+            type="text"
+            placeholder="Please select .."
+            onChange={(e) =>
+              setInput({ ...input, gender: e.currentTarget.value })
+            }
+          >
             <option value="" disabled selected hidden>
               -- Select Gender --
             </option>
@@ -37,18 +62,33 @@ function App() {
 
         <div className="field">
           <label className="label">Age</label>
-          <input className="input" type="number" placeholder="e.q 5"></input>
+          <input
+            className="input"
+            type="number"
+            placeholder="e.q 5"
+            onChange={(e) => setInput({ ...input, age: e.currentTarget.value })}
+          ></input>
         </div>
 
-        <button className="button is-danger is-fullwidth">Submit</button>
+        <button className="button is-danger is-fullwidth" onClick={addform}>
+          Submit
+        </button>
 
         <div className="mb-4"></div>
 
         {/* display tables for all persons */}
         <p className="is-4 title has-text-centered">Pet List</p>
         {/* sample table */}
+
+        {/* {pet.map((item) => (
+          <ItemTable props={item} />
+        ))} */}
+        {/* {pet.map((item) => (
+          console.log("test")
+        ))} */}
         <ItemTable name={"Coco"} gender={"Male"} age={"5"} />
-        <p>Your name and code here</p>
+
+        <p>Wisarud Wongta 620610808</p>
       </div>
     </div>
   );
